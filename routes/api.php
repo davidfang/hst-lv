eb300e57d1e0564fc002c8e1a545c0bd9b894049
515074431 <515074431@qq.com>
2018-07-12 15:57:05 +0800

    laravel admin ok

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::get('test', function () {
+    return 'hello world';
+});
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
